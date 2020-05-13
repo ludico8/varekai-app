9a150e4103c2ef608fad33c8c5fc0068df277281
Diego Corrales <ludico8@gmail.com>
2020-05-12 22:07:34 -0700

    Force to use Https in production

--- server/app.js
+++ server/app.js
@@ -19,6 +19,15 @@ app.get('/*', (req, res, next) => {
   res.sendFile(path.join(__dirname, '..', 'index.html'))
 })
 
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
 // Handle 404s
 app.use((req, res, next) => {
   const err = new Error('Not Found')
